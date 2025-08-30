ba5237b564f2e316be4f91796d96fabc3574ac2a
volder_mort <chetandagajipatil333@gmail.com>
2025-08-31 00:15:50 +0530

    converted dtos to records - ApiResponse and Todo

--- src/main/java/com/learn/restapibasic/ApiResponse.java
+++ src/main/java/com/learn/restapibasic/ApiResponse.java
@@ -1,37 +1,5 @@
 package com.learn.restapibasic;
 
-public class ApiResponse<T> {
-    private boolean isSuccess;
-    private T data;
-    private String errorMessage;
-
-    public ApiResponse(boolean isSuccess, T data, String errorMessage) {
-        this.isSuccess = isSuccess;
-        this.data = data;
-        this.errorMessage = errorMessage;
-    }
-
-    public boolean isSuccess() {
-        return isSuccess;
-    }
-
-    public void setSuccess(boolean success) {
-        isSuccess = success;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
-    }
-}
+public record ApiResponse<T>(
+        boolean isSuccess, T data, String errorMessage
+){}
