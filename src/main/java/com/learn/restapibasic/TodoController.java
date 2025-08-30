d5635665b9056cf29a941e9d56090626048c04e9
volder_mort <chetandagajipatil333@gmail.com>
2025-08-31 00:21:54 +0530

    added versioning for todo controller

--- src/main/java/com/learn/restapibasic/TodoController.java
+++ src/main/java/com/learn/restapibasic/TodoController.java
@@ -13,6 +13,7 @@ import java.util.*;
  *   send response - whatever we have returned that is automatically converted into response object
  * */
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController {
     private static List<Todo> todoList;
 
@@ -26,12 +27,12 @@ public class TodoController {
         todoList.add(new Todo(4, true, "Todo 2", 1));
     }
 
-    @GetMapping("/todos")
+    @GetMapping("/")
     public ResponseEntity<ApiResponse<List<Todo>>> getTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(true, todoList, null));
     }
 
-    @GetMapping("/todos/{todoID}")
+    @GetMapping("/{todoID}")
     public ResponseEntity<ApiResponse<Todo>> getTodo(@PathVariable int todoID){
         for(Todo todo : todoList){
             if(todo.getId() == todoID){
@@ -41,7 +42,7 @@ public class TodoController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, null, "todo with id : " + todoID + " not found"));
     }
 
-    @PostMapping("/todos")
+    @PostMapping("/")
 //    @ResponseStatus(HttpStatus.CREATED) - can be used to set status code in case of succeses
     public ResponseEntity<ApiResponse<Todo>> createTodo(@RequestBody Todo newTodo){
         todoList.add(newTodo);
